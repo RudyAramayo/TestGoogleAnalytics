05c43db9c8f6b59790ca6b3d873ea501829707c1
RudyAramayo <rudy@klinkcdc.com>
2013-03-26 12:08:58 -0500

    Initial Commit

---
+++ TestGoogleAnalytics/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestGoogleAnalytics
+//
+//  Created by Villela Medina on 3/26/13.
+//  Copyright (c) 2013 OrbitusRobotics. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TestGoogleAnalytics/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  TestGoogleAnalytics
+//
+//  Created by Villela Medina on 3/26/13.
+//  Copyright (c) 2013 OrbitusRobotics. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
